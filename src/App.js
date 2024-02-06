4bdefa36d0b2f1c54f3d07898b7f95eb93c59048
Nitis <nitis1203@gmail.com>
2024-02-06 17:17:29 +0530

    Meta tags setup and Header and Nav Bar setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import Header from './Header';
+import Nav from './Nav';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Header/>
+      <Nav />
     </div>
   );
 }
